a82c8e43517e4a9bb9f43894b14f49aca864d6ae
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2020-09-05 14:14:55 -0500

    version app update ionic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AppUpdate } from '@ionic-native/app-update/ngx';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private appUpdate: AppUpdate
   ) {
     this.initializeApp();
   }
@@ -22,6 +24,16 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      const updateUrl = 'https://geniusfitwatch.com/wp-content/uploads/my_app_update.xml';
+      this.appUpdate.checkAppUpdate(updateUrl).then(update => {
+        console.log(update.msg);
+
+      }).catch(error => {
+        console.log('error: ', error);
+
+      });
+
     });
   }
 }
