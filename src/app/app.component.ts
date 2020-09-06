09636889d72291c8972d6a74838e9e8973f0f2f5
Jimy Joaquin Huacho Dolores <jimmyhd_19@hotmail.com>
2020-09-06 01:31:00 -0500

    Deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-
+import { Deploy } from "cordova-plugin-ionic";
 import { Platform } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
@@ -12,7 +12,7 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
+    private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
@@ -20,7 +20,30 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.performAutomaticUpdate();
       this.splashScreen.hide();
     });
+  }
+  async performAutomaticUpdate() {
+    try {
+      const currentVersion = await Deploy.getCurrentVersion();
+      console.log("currentVersion:: ", currentVersion);
+
+      const resp = await Deploy.sync(
+        { updateMethod: "auto" },
+        (percentDone) => {
+          console.log(`Update is ${percentDone}% done!`);
+        }
+      );
+      console.log("resp", resp);
+
+      if (!currentVersion || currentVersion.versionId !== resp.versionId) {
+        // We found an update, and are in process of redirecting you since you put auto!
+      } else {
+        // No update available
+      }
+    } catch (err) {
+      // We encountered an error.
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouteReuseStrategy } from "@angular/router";
-
 import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { Pro } from "@ionic/pro";
-
-Pro.init("ef436209", {
-  appVersion: "0.0.6",
-});
 
 @NgModule({
   declarations: [AppComponent],
