0ff5ac0ac9f261c52d145963a5efb03de94fcf60
Jimy Joaquin Huacho Dolores <jimmyhd_19@hotmail.com>
2020-09-06 16:33:00 -0500

    finalizando demo app center

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { CodePush } from "@ionic-native/code-push/ngx";
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
+  progress = "";
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private codePush: CodePush
   ) {
     this.initializeApp();
   }
@@ -22,6 +25,36 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.checkCodePush();
     });
+  }
+
+  checkCodePush() {
+    this.codePush
+      .sync(
+        {
+          updateDialog: {
+            mandatoryUpdateMessage: "Existe una nueva actualización",
+            appendReleaseDescription: true,
+            descriptionPrefix: "\n\nChange log:\n",
+          },
+          installMode: 0,
+        },
+        (downloadProgress) => {
+          this.progress =
+            "Dowload: " +
+            downloadProgress.receivedBytes +
+            " of " +
+            downloadProgress.totalBytes;
+        }
+      )
+      .subscribe(
+        (data) => {
+          console.log("data:: ", data);
+        },
+        (error) => {
+          console.log("error:: ", error);
+        }
+      );
   }
 }
